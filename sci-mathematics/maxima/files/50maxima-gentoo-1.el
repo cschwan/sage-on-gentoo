398cad57b8fcaba669d79f974556e71f3aa712e2
François Bissey <francois.bissey@canterbury.ac.nz>
2017-01-23 14:31:31 +1300

    sci-mathematics/maxima: new maxima - hopefully for sage 7.6. Revision for 5.35.1 as imaxima is masked. Not sure it works if you have emacs.
    Package-Manager: portage-2.3.0

--- sci-mathematics/maxima/files/50maxima-gentoo-1.el
+++ sci-mathematics/maxima/files/50maxima-gentoo-1.el
@@ -1,6 +1,3 @@
-
-;;; maxima site-lisp configuration
-
 (add-to-list 'load-path "@SITELISP@")
 (autoload 'maxima-mode "maxima" "Maxima mode" t)
 (autoload 'maxima "maxima" "Maxima interactive" t)
@@ -10,3 +7,7 @@
 ;; emaxima mode
 (autoload 'emaxima-mode "emaxima" "EMaxima" t)
 (add-hook 'emaxima-mode-hook 'emaxima-mark-file-as-emaxima)
+
+;; imaxima
+(autoload 'imaxima "imaxima" "Image support for Maxima." t)
+(autoload 'imath-mode "imath" "Interactive Math minor mode." t)
