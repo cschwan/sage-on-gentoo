fe55267ac323f4b96e283965749db1f6877fd84e
François Bissey <frp.bissey@gmail.com>
2024-08-12 13:59:09 +1200

    check arguments, check package existence in main tree

--- tools/suitesparse/Sync_to_gentoo.sh
+++ tools/suitesparse/Sync_to_gentoo.sh
@@ -3,6 +3,13 @@
 # - gentoo overlay path as the departure point
 # - path to Gentoo tree fork as the destination
 
+if [ $# -ne 2 ]; then
+	echo "You need 2 arguments"
+	echo "1) path to overlay containing the new ebuild"
+        echo "2) path to to gentoo tree/fork to update"
+	exit 1
+fi
+
 sog_overlay=$1
 gentoo_dest=$2
 
@@ -26,8 +33,16 @@ Sparse_ebuild=(
 
 for pkg in "${Sparse_ebuild[@]}"; do
         echo "doing ${pkg}"
-        rsync -rv --exclude=Manifest --exclude=metadata.xml "${sog_overlay}/sci-libs/${pkg}/" "${gentoo_dest}/sci-libs/${pkg}/"
-        pushd "${gentoo_dest}/sci-libs/${pkg}/"
-        ebuild *.ebuild manifest
-        popd
+        # checking if pkg is in the gentoo tree. do not add if not.
+        if [ ! -d "${gentoo_dest}/sci-libs/${pkg}/" ]; then
+                rsync -rv --exclude=Manifest --exclude=metadata.xml "${sog_overlay}/sci-libs/${pkg}/" "${gentoo_dest}/sci-libs/${pkg}/"
+                # update the manifest
+                pushd "${gentoo_dest}/sci-libs/${pkg}/"
+                ebuild *.ebuild manifest
+                popd
+        else
+                echo "sci-libs/${pkg} doesn't exist in the main tree, It needs to be added separately as a new ebuild."
+        fi
 done
+
+echo "all done"
